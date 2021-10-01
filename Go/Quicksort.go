3e0d96c9d1799ce1fd4f8371e15eb07139c53477
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2021-10-01 14:26:36 +0530

    Added comments

--- Go/Quicksort.go
+++ Go/Quicksort.go
@@ -1,3 +1,11 @@
+//Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of quickSort that pick pivot in different ways. 
+
+// Best Time Complexity : O(NlogN)
+// Average Time Complexity : O(NlogN)
+// Worst Time Complexity : O(N^2)
+
+//Space Complexity: O(N)
+
 package main
 
 import (
