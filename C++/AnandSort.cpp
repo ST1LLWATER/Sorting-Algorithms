4f81f917953b0d5c67c0d7e9f0ffb4a9cd323648
Anand Singh <59029929+anand722000@users.noreply.github.com>
2021-10-04 12:50:26 +0530

    Update AnandSort.cpp

--- C++/AnandSort.cpp
+++ C++/AnandSort.cpp
@@ -1,7 +1,9 @@
+/* This Sorting Algorith is based on array index matching, if element matches with the index number then that element is assigned at that place or index of array */
+/* if we will assign each element at unique index and then print array in increasing order, ofcorse element will be printed in ascending order */
 #include<iostream>
 using namespace std;
 int main(){
-int i,j,n,A[100]={0};
+int i,j,n,A[100]={0};   // Initally each index is assigned with 0
 cout<<"Enter Size Of the Array: ";
 cin>>n;
 int B[n];
@@ -17,8 +19,9 @@ for(i=0;i<n;i++){
     }
 }
 for(i=0;i<100;i++){
-    if(A[i]!=0){
+    if(A[i]!=0){      //It will print only that index element value which is not having 0 
 cout<<A[i]<<" "; }
 }
 return 0;
 }
+        
